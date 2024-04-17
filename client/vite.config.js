6caa78c96c93391d042d8b3fe304fbbc32cd6d06
MesQue10 <codse221f-083@student.nibm.lk>
2024-04-18 00:17:27 +0530

    working one

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createVuePlugin } from 'vite-plugin-vue';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer'; // Import autoprefixer
 
 export default defineConfig({
-  plugins: [
-    react(),
-    createVuePlugin() // Include this if you're using Vue.js as well
-  ],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
+        tailwindcss,
+        autoprefixer // Use autoprefixer directly
       ],
     },
   },
