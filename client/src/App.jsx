5095cfec5465476fff42c185a3f514e24704c414
MesQue10 <codse221f-083@student.nibm.lk>
2024-04-20 13:02:49 +0530

    idk2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import {Route, Routes} from "react-router-dom";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
+import {LoginPage} from './pages/LoginPage';
 
 function App() {
-
   return (
     <Routes>
-      <Route index element={<IndexPage/>}/>
+      <Route index element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
-   
-  )
+  );
 }
 
-export default App
+export default App;
