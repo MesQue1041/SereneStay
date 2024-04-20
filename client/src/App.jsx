11fc5cb77942ca9d6d0e440c137bd2fd2ae5ff62
MesQue10 <codse221f-083@student.nibm.lk>
2024-04-20 13:59:28 +0530

    register ui almsot done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import { LoginPage } from './pages/LoginPage'; // Import LoginPage as a named export
 import { Layout } from './Layout'; // Import Layout as a named export
+import { RegisterPage } from './pages/RegisterPage'; // Import RegisterPage as a named export
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}> 
         <Route index element={<IndexPage />} /> 
         <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/register" element={<RegisterPage />} /> 
       </Route>
     </Routes>
   );
